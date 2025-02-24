7cd3b58d8ab291628e60dab0192d0bdcce16b914
Smolentsev Dmitrii <smolentsev_dn@icloud.com>
2025-02-24 10:24:04 +0600

    refactor(typography): use Inter as main font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,9 @@
+import { ReactNode } from 'react'
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const inter = Inter({
   subsets: ['latin'],
 })
 
@@ -20,11 +15,11 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: ReactNode
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   )
 }

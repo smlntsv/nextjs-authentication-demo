474603f081b37c7c60ab825ca47d0d962dd43ae9
Smolentsev Dmitrii <smolentsev_dn@icloud.com>
2025-02-25 10:49:52 +0600

    fix(storybook): fix background support in docs view mode

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,6 +1,13 @@
 import type { Preview } from '@storybook/react'
 import '@/app/globals.css'
 
+type BackgroundValue = { name: string; value: string }
+
+const backgroundValues: BackgroundValue[] = [
+  { name: 'Light', value: '#ffffff' },
+  { name: 'Dark', value: '#1e293b' },
+]
+
 const preview: Preview = {
   parameters: {
     controls: {
@@ -9,7 +16,46 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    backgrounds: {
+      values: backgroundValues,
+      default: backgroundValues[0].name,
+    },
   },
+  decorators: [
+    (story, context) => {
+      const defaultBackgroundColor = backgroundValues[0].value
+      const currentBackgroundColor = context.globals.backgrounds?.value ?? defaultBackgroundColor
+
+      if (
+        !context.globals.backgrounds ||
+        context.globals.backgrounds.value === defaultBackgroundColor
+      ) {
+        document.documentElement.style.setProperty('color-scheme', 'light')
+        document.documentElement.setAttribute('data-theme', 'light')
+      } else {
+        document.documentElement.style.setProperty('color-scheme', 'dark')
+        document.documentElement.setAttribute('data-theme', 'dark')
+      }
+
+      // Fix for https://github.com/storybookjs/storybook/issues/13323
+      if (context.viewMode === 'docs') {
+        let currentElement = context.canvasElement
+
+        while (currentElement?.parentElement) {
+          if (currentElement.classList.contains('docs-story')) {
+            currentElement.style.background = currentBackgroundColor
+            break
+          }
+
+          currentElement = currentElement.parentElement
+        }
+
+        return story()
+      }
+
+      return story()
+    },
+  ],
 }
 
 export default preview

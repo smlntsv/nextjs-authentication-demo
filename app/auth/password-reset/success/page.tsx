091d055a7ae4674439aa623e5c717451035583f6
Smolentsev Dmitrii <smolentsev_dn@icloud.com>
2025-03-03 16:45:52 +0600

    feat(app): style password reset success page

--- app/auth/password-reset/success/page.tsx
+++ app/auth/password-reset/success/page.tsx
@@ -1,13 +1,22 @@
-import Link from 'next/link'
+import { LinkButton } from '@/components/ui/button'
+import { Container } from '@/components/ui/container'
+import { Card, CardHeading, CardText } from '@/components/card/card'
+import { IconKey } from '@/components/icons/icon-key'
+import styles from './page.module.css'
 
 const PasswordResetSuccessPage = () => (
-  <div>
-    <h1>Password Reset</h1>
-    <p>Your password has been successfully reset.</p>
-    <p>
-      <Link href="/auth/sign-in">Go to sign in</Link>
-    </p>
-  </div>
+  <Container centered>
+    <Card icon={<IconKey />}>
+      <CardHeading>Password Updated</CardHeading>
+      <CardText>
+        Your password has been successfully updated.
+        <br /> Click below to go to the login page.
+      </CardText>
+      <LinkButton href={'/auth/sign-in'} className={styles.continueButton}>
+        Go to Login Page
+      </LinkButton>
+    </Card>
+  </Container>
 )
 
 export default PasswordResetSuccessPage

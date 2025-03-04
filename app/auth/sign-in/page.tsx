2f7005687f320a73a12f8f0cb2e44f784def11c0
Smolentsev Dmitrii <smolentsev_dn@icloud.com>
2025-03-04 10:29:45 +0600

    feat(components): style sign-in page

--- app/auth/sign-in/page.tsx
+++ app/auth/sign-in/page.tsx
@@ -1,23 +1,37 @@
+import { Container } from '@/components/ui/container'
+import { Text } from '@/components/ui/typography/text'
 import { signInAction } from '@/lib/auth/actions/sign-in-action'
-import { AuthForm } from '@/app/auth/auth-form'
-import Link from 'next/link'
+import { AuthForm } from '@/components/auth-form'
+import { LinkButton } from '@/components/ui/button'
+import { Card, CardHeading, CardText } from '@/components/card'
+import { IconKey } from '@/components/icons/icon-key'
+import styles from './page.module.css'
 
 const SignInPage = () => {
   return (
-    <div>
-      <h1>Log in to your account</h1>
-      <AuthForm
-        action={signInAction}
-        submitButtonText={'Sign in'}
-        passwordAutocomplete={'current-password'}
-      />
-      <div>
-        <Link href={'/auth/sign-up'}>Sign Up</Link>
-      </div>
-      <div>
-        <Link href={'/auth/password-reset'}>Reset Password</Link>
-      </div>
-    </div>
+    <Container centered>
+      <Card icon={<IconKey />}>
+        <CardHeading>Log in to your account</CardHeading>
+        <CardText>Welcome back! Please enter your details.</CardText>
+        <AuthForm
+          className={styles.form}
+          action={signInAction}
+          submitButtonText={'Sign In'}
+          passwordAutocomplete={'current-password'}
+        />
+        <div className={styles.bottomLinks}>
+          <Text size={'sm'} className={styles.bottomLinksText}>
+            Don&#39;t have an account?{' '}
+            <LinkButton href={'/auth/sign-up'} variant={'linkColor'}>
+              Sign Up
+            </LinkButton>
+          </Text>
+          <LinkButton href={'/auth/password-reset'} variant={'linkColor'}>
+            Forgot password
+          </LinkButton>
+        </div>
+      </Card>
+    </Container>
   )
 }
 
